4385104fb3daad9260ddd2f05ea45b09501a7e49
PTCKeshawn <keshawnchidionor@gmail.com>
2026-01-23 15:25:12 -0600

    Started creating the game function

--- rock_paper_scissors.sh
+++ rock_paper_scissors.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-options = ["Rock", "Scissors", "Paper" ]
+options=(Rock Scissors Paper)
 
 main_menu(){
 	echo "Welcome to Rock paper scissors frenzy!"
 	sleep 1
-	play_options
-}
-
-
-play_options(){
 	echo -e "would you like to play: 
 	1) first to 1
 	2) first to 3
 	3) first to 5
 	4) custom play (you pick what its up to)
-	5) freeplay"
+	5) freeplay
+	6) exit "
 
 	read -p ":" choice
+	game
 }
+
+game(){
+	for loop in 
+}
+main_menu
