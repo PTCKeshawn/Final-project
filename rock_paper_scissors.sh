38d83483b48d860aeebb3b1cc7b5fc1da84abdbf
PTCKeshawn <keshawnchidionor@gmail.com>
2026-01-26 12:13:11 -0600

    Reused the for loop and fixed the issue. added more to the game function.

--- rock_paper_scissors.sh
+++ rock_paper_scissors.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-options=(Rock Scissors Paper)
+options=(rock scissors paper)
+your_score=0
+ai_score=0
+
+echo "Welcome to Rock paper scissors frenzy!"
 
 main_menu(){
-	echo "Welcome to Rock paper scissors frenzy!"
 	sleep 1
 	echo -e "would you like to play: 
-	1) first to 1
-	2) first to 3
-	3) first to 5
+	1) best of 1
+	2) best of 3
+	3) best of 5
 	4) custom play (you pick what its up to)
 	5) freeplay
 	6) exit "
@@ -18,13 +21,16 @@ main_menu(){
 }
 
 check_choice(){
-	case choice in
-		1) game_to_1
+	case $choice in
+		1) choice=1
+			game_runner
 			;;
-		2) game_to_3
+		2) choice=3
+			game_runner
+			;;						
+		3) choice=5
+			game_runner
 			;;
-		3) game_to_5
-			;;			
 		4) read -p "first to : " choice 
 			echo "game is first to $choice"
 			game_runner
@@ -37,11 +43,45 @@ check_choice(){
 	esac
 }
 
-#game_runner(){
-#	for (( i = 0; i <= choice ; i++ )); do
-#		game
-#	done
-#	echo "sddsgj"
-#}
+game_runner(){
+	for (( i = 1; i <= choice ; i++ )); do
+		game
+		if [[ $your_score == $ai_score ]]; then
+			echo "tiebreaker!"
+			game
+	done
+	main_menu
+	echo "sddsgj"
+}
+
+
+game(){
+	read -p "pick between rock (r), paper (p), scissors (s) : " selection
+	ai_item=$(printf "%s\n" "${options[@]}" | shuf -n 1)
+		if [[ ( $selection == "r" || $selection == "R" ) && $ai_item == "scissors" ]]; then
+			echo -e "you picked rock
+I picked scissors"
+			sleep 1
+			echo "You win!"
+			((your_score++))
+			echo -e "your score = $your_score
+ai score = $ai_score"
+			echo $your_score
+		elif [[ ( $selection == "r" || $selection == "R" ) && $ai_item == "rock" ]]; then
+			echo -e "you picked rock
+			I picked rock"
+			echo "Tie!"
+			echo -e "your score = $your_score
+ai score = $ai_score"
+		elif [[ ( $selection == "r" || $selection == "R" ) && $ai_item == "paper" ]]; then
+			echo -e "you picked rock
+			I picked paper"
+			echo "I win!"
+			((ai_score++))
+			echo -e "your score = $your_score
+ai score = $ai_score"
+		fi
+		echo "$ai_item"
+}
 
 	main_menu
